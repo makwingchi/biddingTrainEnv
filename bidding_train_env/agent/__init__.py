1e02dadd903964c06a277613078e472a169386b2
rongzhimai <rongzhimai@whale.im>
2024-02-06 16:06:30 +0800

    implement onehot category agent and dataloader

--- bidding_train_env/agent/__init__.py
+++ bidding_train_env/agent/__init__.py
@@ -1,4 +1,5 @@
 # from .player_agent import PlayerAgent as PlayerAgent
 # from .iql_agent import IqlAgent as PlayerAgent
 # from .bc_agent import BcAgent as PlayerAgent
-from .iql_agent_category import IqlAgent as PlayerAgent
+# from .iql_agent_category import IqlAgent as PlayerAgent
+from .iql_agent_onehot_category import IqlAgent as PlayerAgent
